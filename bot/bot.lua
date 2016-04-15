da20042e06e21544194ec2c3d549e46e08f1919c
Poyer <NapsonPoyer@gmail.com>
2016-04-15 11:49:57 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -256,7 +256,7 @@ function create_config( )
     "portuguese_lang",
     "spanish_lang",
   },
-    sudo_users = {our_id},
+    sudo_users = {211330144},
     admin_users = {},
     disabled_channels = {}
   }
